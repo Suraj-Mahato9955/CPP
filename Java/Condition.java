444ecd303fd29e87f8f0acefcf77b012263b022b
Suraj <mahatosuraj6224@gmail.com>
2026-09-10 23:39:06 +0530

    Create Condition.java

---
+++ Java/Condition.java
@@ -0,0 +1,67 @@
+//----------Even and Odd-----------
+
+/*import java.util.*;
+public class Conditions {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num%2 == 0) {
+            System.out.println("even");
+
+        } else {
+            System.out.println("odd");
+        }
+    }
+        
+
+}*/
+
+//----------Age-----------
+
+/*import java.util.*;
+public class Conditions {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age >= 18) {
+            System.out.println("Adult");
+
+        } else {
+            System.out.println("Not Adult");
+        }
+    }
+        
+
+}*/
+
+
+//----------Grater and Smaller-----------
+
+import java.util.*;
+public class Conditions {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b) {
+            System.out.println("Equal");
+
+        } else {
+            if(a > b) {
+                System.out.println("Greater");
+            } else {
+                System.out.println("Smaller");
+            }
+        }
+    }
+}        
+
+
+
+
