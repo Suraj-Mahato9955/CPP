84cbc8cdfc5e01cb4b098b1ffd36d9cbf5dff3ed
Suraj <mahatosuraj6224@gmail.com>
2026-09-14 22:59:07 +0530

    Update Condition.java

--- Java/Condition.java
+++ Java/Condition.java
@@ -44,6 +44,7 @@ public class Conditions {
 import java.util.*;
 public class Conditions {
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -61,6 +62,32 @@ public class Conditions {
         }
     }
 }        
+
+/*int n = 1;
+switch(n) {
+   case 1 :
+       System.out.println("Monday");
+       break;
+   case 2 :
+       System.out.println("Tuesday");
+       break;
+   case 3 :
+       System.out.println("Wednesday");
+       break;
+   case 4 :
+       System.out.println("Thursday");
+       break;
+   case 5:
+       System.out.println("Friday");
+       break;
+   case 6 :
+       System.out.println("Saturday");
+       break;
+   default :
+       System.out.println("Sunday");
+}
+
+ */
 
 
 
